3e8b73e6b5cc6a850d22caebe2bc952052061ea6
KuroloffS <s.qurolov@newuu.uz>
2025-03-06 08:36:32 +0500

    lesson-9

---
+++ lesson-9/homework/task3.sql
@@ -0,0 +1,9 @@
+with fibonacci(n, Fibonacci_number, Prev) as 
+(
+    select 1, 1, 0
+    union ALL
+    select n + 1, Fibonacci_number + Prev, Fibonacci_number
+    from fibonacci
+    where n < 10
+)
+select n, Fibonacci_number from fibonacci;
